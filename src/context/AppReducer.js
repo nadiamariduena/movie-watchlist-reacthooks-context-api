7a6fb2e637cc079d85bae20443c71463679d808a
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-02-20 23:57:53 +0100

    reducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,11 @@
 export default (state, action) => {
   switch (action.type) {
+    case "ADD_MOVIE_TO_WATCHLIST":
+      return {
+        // 1  here we will return the existing state "...state"
+        ...state,
+        //2 and then we want to make some changes to the state
+      };
     default:
       return state;
   }
